a85e18590a828123edd26b6ab519457de2563229
Anitacas <66044381+Anitacas@users.noreply.github.com>
2020-08-18 20:09:01 -0600

    Notificaciones en primer plano
    Signed-off-by: Anitacas <66044381+Anitacas@users.noreply.github.com>

--- app/src/main/java/com/example/homelessanimals/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/homelessanimals/MyFirebaseMessagingService.kt
@@ -1,6 +1,20 @@
 package com.example.homelessanimals
-
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.widget.Toast
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
 
 class MyFirebaseMessagingService: FirebaseMessagingService() {
+
+   override fun onMessageReceived(remoteMessage: RemoteMessage) {
+
+     Looper.prepare()
+      Handler().post {
+         Toast.makeText(baseContext, remoteMessage.notification?.title, Toast.LENGTH_LONG).show()
+      }
+      Looper.loop()
+   }
 }
